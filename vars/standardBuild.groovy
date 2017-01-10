77c15b9370f792412e544b02cd556b544d051dc8
ahridin <ahridin@gmail.com>
2017-01-10 02:25:15 +0200

    Update standardBuild.groovy

--- vars/standardBuild.groovy
+++ vars/standardBuild.groovy
@@ -5,7 +5,9 @@ def call(body) {
     body()
     node {
         stage('Checkout') {
-            checkout scm
+            //checkout scm
+            checkout([$class: 'SubversionSCM', additionalCredentials: [], excludedCommitMessages: '', excludedRegions: '', excludedRevprop: '', excludedUsers: '', filterChangelog: false, ignoreDirPropChanges: false, includedRegions: '', locations: [[credentialsId: '29bae92d-6b9c-4f76-a54e-5b72f851a397', depthOption: 'infinity', ignoreExternalsOption: false, local: '.', remote: 'https://wwwin-svn-jrsm.cisco.com/nds/ch_repo/tags/vgs3/acman']], workspaceUpdater: [$class: 'CheckoutUpdater']])
+
         }
         stage('Main') {
             docker.image(config.environment).inside {
